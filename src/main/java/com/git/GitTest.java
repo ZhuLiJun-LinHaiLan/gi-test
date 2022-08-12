f176f2cf226887d5625039193ebd2c07e198b2d9
ZhuLiJun <3551817930@qq.com>
2022-08-12 08:24:54 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * 作者：zhulijun
+ * 日期：2022/08/12/8:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
